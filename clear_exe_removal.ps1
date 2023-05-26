5e20a7abac55493afe0f9c039e86ac6e0a5eaf6d
Muhammad Talha <50596053+muhammadtalha01@users.noreply.github.com>
2023-05-26 14:02:41 +0400

    Threat removal Scripts

---
+++ clear_exe_removal.ps1
@@ -0,0 +1,45 @@
+﻿# Stop Clear/Clearbrowser processes
+if (Get-Process -Name Clear -ErrorAction SilentlyContinue) {
+   Write-Output "Clear Processes found...terminating"
+   Stop-Process -Name Clear -Force -ErrorAction SilentlyContinue
+}
+else {
+   Write-Output "No Clear Processs found"
+}
+if (Get-Process -Name clearbrowser -ErrorAction SilentlyContinue) {
+   Write-Output "clearbrowser Processes found...terminating"
+   Stop-Process -Name clearbrowser -Force -ErrorAction SilentlyContinue
+}
+else {
+   Write-Output "No clearbrowser Processs found"
+}
+# Remove Clear Directory and files
+if ($clearFolder1 = Get-Item "C:\Users\*\AppData\Local\Programs\Clear*" -ErrorAction SilentlyContinue) {
+   Write-Output "Clear found at $($clearFolder1.FullName)...removing"
+   Remove-Item "C:\Users\*\AppData\Local\Programs\Clear*" -Force -Recurse -ErrorAction SilentlyContinue
+}
+else {
+   Write-Output "No Clear files found in 'C:\Users\*\AppData\Local\Programs\Clear*'"
+}
+if ($clearFolder2 = Get-Item "C:\Users\*\Downloads\Clear*" -ErrorAction SilentlyContinue) {
+   Write-Output "Clear found at $($clearFolder2.FullName)...removing"
+   Remove-Item "C:\Users\*\Downloads\Clear*" -Force -Recurse -ErrorAction SilentlyContinue
+}
+else {
+   Write-Output "No Clear files found in 'C:\Users\*\Downloads\Clear*'"
+}
+# Remove Scheduled Task
+if (Get-ScheduledTask -TaskName ClearbarStartAtLoginTask* -ErrorAction SilentlyContinue) {
+   Write-Output "Scheduled task found...removing"
+   Unregister-ScheduledTask -TaskName ClearbarStartAtLoginTask* -confirm:$false -ErrorAction SilentlyContinue
+}
+else {
+   Write-Output "ClearbarStartAtLoginTask scheduled task was not found"
+}
+if (Get-ScheduledTask -TaskName ClearbarUpdateChecker* -ErrorAction SilentlyContinue) {
+   Write-Output "Scheduled task found...removing"
+   Unregister-ScheduledTask -TaskName ClearbarUpdateChecker* -confirm:$false -ErrorAction SilentlyContinue
+}
+else {
+   Write-Output "ClearbarUpdateChecker* scheduled task was not found"
+}
---
+++ wave_browser_removal.ps1
@@ -0,0 +1,45 @@
+﻿# Stop Wave Browser Processes
+if (Get-Process -Name wavebrowser -ErrorAction SilentlyContinue) {
+   Write-Output "wavebrowser Processes found...terminating"
+   Stop-Process -Name wavebrowser -Force -ErrorAction SilentlyContinue
+}
+else {
+   Write-Output "No wavebrowser Processs found"
+}
+# Remove wavebrowser Directory and files
+if ($wavebrowserFolder1 = Get-Item "C:\Users\*\AppData\Local\wavebrowser*" -ErrorAction SilentlyContinue) {
+   Write-Output "wavebrowser found at $($wavebrowserFolder1.FullName)...removing"
+   Remove-Item "C:\Users\*\AppData\Local\wavebrowser*" -Force -Recurse -ErrorAction SilentlyContinue
+}
+else {
+   Write-Output "No wavebrowser files found in 'C:\Users\*\AppData\Local\wavebrowser*'"
+}
+if ($wavebrowserFolder2 = Get-Item "C:\Users\*\Wavesor Software*" -ErrorAction SilentlyContinue) {
+   Write-Output "wavebrowser found at $($wavebrowserFolder2.FullName)...removing"
+   Remove-Item "C:\Users\*\Wavesor Software*" -Force -Recurse -ErrorAction SilentlyContinue
+}
+else {
+   Write-Output "No wavebrowser files found in 'C:\Users\*\Wavesor Software*'"
+}
+if ($wavebrowserDownload = Get-Item "C:\Users\*\Downloads\Wave Browser_*" -ErrorAction SilentlyContinue) {
+   Write-Output "wavebrowser installers found at $($wavebrowserDownload.FullName)...removing"
+   Remove-Item "C:\Users\*\Downloads\Wave Browser_*" -Force -Recurse -ErrorAction SilentlyContinue
+}
+else {
+   Write-Output "No wavebrowser files found in 'C:\Users\*\Downloads*'"
+}
+# Remove Scheduled Task
+if (Get-ScheduledTask -TaskName WavesorSWUpdater* -ErrorAction SilentlyContinue) {
+   Write-Output "Scheduled task found...removing"
+   Unregister-ScheduledTask -TaskName WavesorSWUpdater* -confirm:$false -ErrorAction SilentlyContinue
+}
+else {
+   Write-Output "WavesorSWUpdater* scheduled task was not found"
+}
+if (Get-ScheduledTask -TaskName WaveBrowser-StartAtLogin* -ErrorAction SilentlyContinue) {
+   Write-Output "Scheduled task found...removing"
+   Unregister-ScheduledTask -TaskName WaveBrowser-StartAtLogin* -confirm:$false -ErrorAction SilentlyContinue
+}
+else {
+   Write-Output "WaveBrowser-StartAtLogin* scheduled task was not found"
+}
